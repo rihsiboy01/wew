c4945d563205aadc6d45f51b788ecc5b03cd6c30
rihsiboy01 <auzcom78@gmail.com>
2024-11-11 15:39:04 +0300

    Add files via upload

---
+++ skrupt.js
@@ -0,0 +1,18 @@
+let a = ['ahmat','komron','mustafo','rihsiboy'] 
+
+let b = []
+let d = []
+ for( item of a){
+    if(item.includes('o')){
+        b.push(item); 
+        
+    }else{
+        d.push(item); 
+    }
+ }
+ console.log(a);
+ console.log(b);
+ console.log(d);
+ 
+ 
+ 
